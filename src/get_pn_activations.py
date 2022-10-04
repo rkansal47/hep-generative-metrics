ebb29d82cf95c560e4837db88c9ff0f68e030a96
rkansal47 <rkansal@cern.ch>
2022-10-04 21:33:44 +0000

    bug fixes

--- src/get_pn_activations.py
+++ src/get_pn_activations.py
@@ -6,10 +6,11 @@ import numpy as np
 
 jets = JetNet(
     "g",
-    data_dir="/Users/raghav/Documents/CERN/gen-models/MPGAN/datasets/",
+    data_dir="/graphganvol/MPGAN/datasets/",
     split_fraction=[1, 0, 0],
     jet_features=None,
-    particle_normalisation=JetNet.fpnd_norm
+    particle_normalisation=JetNet.fpnd_norm,
+    particle_features=["etarel", "phirel", "ptrel"],
 )
 
 jets_loaded = DataLoader(jets, shuffle=False, batch_size=256, pin_memory=True)
@@ -21,5 +22,7 @@ for i, jets_batch in tqdm(
     activations.append(jetnet.evaluation.particlenet._ParticleNet(jets_batch.to("cuda"), ret_activations=True).cpu().detach().numpy())
 
 activations = np.concatenate(activations, axis=0)
+
+np.save("activations", activations)
 
 
