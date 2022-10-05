2354a51b4947fe35d81a2abad88fc22f57b4db26
rkansal47 <raghavk1997@yahoo.co.in>
2022-10-05 11:58:08 +0200

    pnet

--- src/get_pn_activations.py
+++ src/get_pn_activations.py
@@ -1,8 +1,9 @@
 from jetnet.datasets import JetNet
+import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
-from particlenet import ParticleNet
+from pnet.particlenet import ParticleNet
 
 jets = JetNet(
     "g",
@@ -15,13 +16,15 @@ jets = JetNet(
 
 jets_loaded = DataLoader(jets, shuffle=False, batch_size=256, pin_memory=True)
 
+pnet = ParticleNet(30, 3).to("gpu")
+pnet.load_state_dict(torch.load(f"pnet/pnet_state_dict.pt", map_location="gpu"))
+pnet.eval()
+
 activations = []
 for i, (jets_batch, _) in tqdm(
     enumerate(jets_loaded), total=len(jets_loaded), desc="Running ParticleNet"
 ):
-    activations.append(
-        ParticleNet(jets_batch.to("cuda"), ret_activations=True).cpu().detach().numpy()
-    )
+    activations.append(pnet(jets_batch.to("cuda"), ret_activations=True).cpu().detach().numpy())
 
 activations = np.concatenate(activations, axis=0)
 np.save("/graphganvol/hep-generative-metrics/pnet_activations.npy", activations)
