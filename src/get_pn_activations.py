7954fc3e50f59632db1bba7cbd13dae743d4cb22
rkansal47 <raghavk1997@yahoo.co.in>
2022-10-12 15:42:09 +0200

    pnet normalisation

--- src/get_pn_activations.py
+++ src/get_pn_activations.py
@@ -29,9 +29,9 @@ pnet.eval()
 
 for key, (jets, _) in pf_dists.items():
     print(key)
-    jets_loaded = DataLoader(
-        jets.astype(np.float32), shuffle=False, batch_size=512, pin_memory=True
-    )
+
+    jets = JetNet.fpnd_norm(jets.astype(np.float32))
+    jets_loaded = DataLoader(jets, shuffle=False, batch_size=512, pin_memory=True)
 
     activations = []
     # for i, (jets_batch, _) in tqdm(
